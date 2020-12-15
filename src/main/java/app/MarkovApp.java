923dd62afedc819420359dc2fff6fa79d8522599
jolignier <42548754+jolignier@users.noreply.github.com>
2020-12-15 14:13:14 +0100

    added Classes Comments

--- src/main/java/app/MarkovApp.java
+++ src/main/java/app/MarkovApp.java
@@ -7,10 +7,21 @@ import java.nio.file.Paths;
 
 import markov.MarkovChain;
 
+/**
+ * Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+ * Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+ * when an unknown printer took a galley of type and scrambled it to make a
+ * type specimen book. It has survived not only five centuries, but also the leap
+ * into electronic typesetting, remaining essentially unchanged. It was
+ * popularised in the 1960s with the release of Letraset sheets containing Lorem
+ * Ipsum passages, and more recently with desktop publishing software like
+ * Aldus PageMaker including versions of Lorem Ipsum
+ */
 public class MarkovApp {
+
 	
 	public static void main(String[] args) throws Exception {
-        String sentences = readFileAsString("./target/VictorHugo_LesMiserables-II-Cosette.txt");
+        String sentences = readFileAsString("./target/classes/VictorHugo_LesMiserables-II-Cosette.txt");
         System.out.println(sentences);
         
         MarkovChain markov = new MarkovChain(3);
@@ -22,7 +33,7 @@ public class MarkovApp {
         System.out.println(markov.generateMarkov(12));
 	}
 
-    public static String readFileAsString(String fileName) throws Exception { 
+    public static String readFileAsString(String fileName) throws Exception {
         String data = ""; 
         Path path = Paths.get(fileName); 
         data = Files.readString(path, StandardCharsets.UTF_8); 
--- src/main/java/markov/MarkovData.java
+++ src/main/java/markov/MarkovData.java
@@ -5,7 +5,16 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+/**
+ * Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+ * Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+ * when an unknown printer took a galley of type and scrambled it to make a
+ * type specimen book. It has survived not only five centuries, but also the leap
+ * into electronic typesetting, remaining essentially unchanged. It was
+ * popularised in the 1960s with the release of Letraset sheets containing Lorem
+ * Ipsum passages, and more recently with desktop publishing software like
+ * Aldus PageMaker including versions of Lorem Ipsum
+ */
 public class MarkovData {
 	private Map<String, String> markovTable = new HashMap<String,String>();
 	private List<String> textwords = new ArrayList<String>();
